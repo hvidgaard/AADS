b2120a04ec32111204c1a68500cce82cc92f4495
Peter Hvidgaard <hvidgaard@gmail.com>
2011-09-07 14:22:32 +0200

    blah4

---
+++ p1_heap/BinaryHeap.cpp
@@ -0,0 +1,23 @@
+#include <AADS.h>
+
+Heap* BinaryHeap::initialize_heap(int size){
+	return 0;
+}
+void BinaryHeap::insert(Element*, Heap*){
+	//nop
+}
+Element* BinaryHeap::find_min(Heap*){
+	return 0;
+}
+Element* BinaryHeap::delete_min(Heap*){
+	return 0;
+}
+Heap* BinaryHeap::meld(Heap*){
+	return 0;
+}
+Element* BinaryHeap::decrease_key(int, Element*){
+	return 0;
+}
+Element* BinaryHeap::delete_element(Element*, Heap*){
+	return 0;
+}
---
+++ p1_heap/FibonacciHeap.cpp
@@ -0,0 +1,23 @@
+#include <AADS.h>
+
+Heap* FibonacciHeap::initialize_heap(int size){
+	return 0;
+}
+void FibonacciHeap::insert(Element*, Heap*){
+	//nop
+}
+Element* FibonacciHeap::find_min(Heap*){
+	return 0;
+}
+Element* FibonacciHeap::delete_min(Heap*){
+	return 0;
+}
+Heap* FibonacciHeap::meld(Heap*){
+	return 0;
+}
+Element* FibonacciHeap::decrease_key(int, Element*){
+	return 0;
+}
+Element* FibonacciHeap::delete_element(Element*, Heap*){
+	return 0;
+}
