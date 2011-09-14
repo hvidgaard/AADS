169ec5160b931ab6dca1a49e14a2a2fa2e09cf3b
Peter Hvidgaard <hvidgaard@gmail.com>
2011-09-14 12:39:43 +0200

    added a few comments

--- p1_heap/BinaryHeap.c
+++ p1_heap/BinaryHeap.c
@@ -11,11 +11,22 @@ unsigned int bh_decrease_key(unsigned int new_key, unsigned int e, Heap *h);
 Element *bh_delete_element(unsigned int e, Heap *h);
 void min_heapify(unsigned int e, Heap *h);
 void bh_exchange(unsigned int e1, unsigned int e2, Heap *h);
-	
-//Parent(i): i / 2
-//Left(i): i * 2
-//Right(i): (i * 2) + 1
 
+/* Given an index in the array, the family relations are as following:
+ * 
+ * Parent(i): i / 2
+ * Left(i): i * 2
+ * Right(i): (i * 2) + 1
+ * 
+ * note this requires that the first element is 1, so in order to not
+ * complicate the family relation formulas, the element at index 0 is
+ * just sitting and never used.
+ */
+
+/* min_heapify repairs the heap after an element is inserted that is larger than
+ * it's children. Basicly it will move it down until both of it's children are
+ * larger - while making sure that it stays within the size of the current heap.
+ */
 void min_heapify(unsigned int e, Heap *h) {
 	unsigned int parent = e / 2;
 	unsigned int left = e * 2;
@@ -40,14 +51,14 @@ void bh_exchange(unsigned int e1, unsigned int e2, Heap *h) {
 	unsigned int t_key = h->data[e1].key;
 	unsigned int t_index = h->data[e1].index;
 	void *t_data = h->data[e1].data;
-	printf("Exchanging %d key %d and %d key %d\n", e1, h->data[e1].key, e2, h->data[e2].key);
+	//printf("Exchanging %d key %d and %d key %d\n", e1, h->data[e1].key, e2, h->data[e2].key);
 	h->data[e1].key = h->data[e2].key;
 	h->data[e1].data = h->data[e2].data;
 	h->data[e1].index = h->data[e2].index;
 	h->data[e2].key = t_key;
 	h->data[e2].data = t_data;
 	h->data[e2].index = t_index;
-	printf("now %d key %d and %d key %d\n\n", e1, h->data[e1].key, e2, h->data[e2].key);
+	//printf("now %d key %d and %d key %d\n\n", e1, h->data[e1].key, e2, h->data[e2].key);
 }
 
 Heap *bh_init_heap(unsigned int size) {
@@ -61,23 +72,23 @@ Heap *bh_init_heap(unsigned int size) {
 unsigned int bh_insert(unsigned int key, void *data, Heap *h) {
 	unsigned int result;
 	if (h->size < h->max_size) {
-		printf("insert key %d\n", key);
+		//printf("insert key %d\n", key);
 		h->size++;
 		h->data[h->size].key = UINT_MAX;
 		h->data[h->size].data = data;
 		h->data[h->size].index = h->size;
 		result = bh_decrease_key(key, h->size, h);
-		printf("inserted key %d, got index %d\n\n", key, result);
+		//printf("inserted key %d, got index %d\n\n", key, result);
 		return result;
 	}
 	else {
-		printf("heap is full\n");
+		//printf("heap is full\n");
 		return 0;
 	}
 }
 
 unsigned int bh_find_min(Heap *h) {
-	if (h->size > 1)
+	if (h->size > 0)
 		return 1;
 	else
 		return 0;
@@ -90,14 +101,19 @@ Element *bh_delete_min(Heap *h) {
 		return NULL;
 }
 
+/* NOT IMPLEMENTED
+ */
 Heap *bh_meld(Heap *h1, Heap *h2) {
 	return 0;
 }
 
+/* will decrease the key of an element, and bubble it up until the heap
+ * property is reestablished
+ */
 unsigned int bh_decrease_key(unsigned int new_key, unsigned int e, Heap *h) {
 	unsigned int parent;
 	if (new_key >= h->data[e].key)
-			//technically it's an error
+			//technically it's an error, so we return 0
 			return 0;
 	if (e <= h->size) {
 		h->data[e].key = new_key;
@@ -113,7 +129,7 @@ unsigned int bh_decrease_key(unsigned int new_key, unsigned int e, Heap *h) {
 
 Element *bh_delete_element(unsigned int e, Heap *h){
 	Element *result = malloc(sizeof(struct Element));
-	printf("Deleting: %d, with key %d\n", e, h->data[e].key);
+	//printf("Deleting: %d, with key %d\n", e, h->data[e].key);
 	if (h->size > 1 && e <= h->size) {
 		bh_exchange(e, h->size, h);
 		h->size--;
