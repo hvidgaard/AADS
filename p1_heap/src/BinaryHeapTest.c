c9c1852109e197fc474f77172ad69cac343fe7e4
Anders Ingemann <anders@ingemann.de>
2011-09-16 02:31:55 +0200

    I have no idea why it complains about the heap being a null reference. Initial compile errors etc. fixed. All source files moved to src. Better ant build.xml

--- p1_heap/src/BinaryHeapTest.c
+++ p1_heap/src/BinaryHeapTest.c
@@ -4,20 +4,9 @@
 
 int main(int argc, char **argv)
 {
-	FibHeap *heap = fib_make_heap(16);
-	int i;
-	for (i = 1; i <= 16; i++)
-		fib_insert(17-i, heap);
-	i = 0;
-	FibNode *min = (FibNode *) fib_find_min(heap);
-	while (min != NULL) {
-		printf("min is %d\n", min->key);
-		fib_delete_min(heap);
-		min = fib_find_min(heap);
-	}
-	return 0;
 	printf("hello world\n");
 	binary_heap *h = bh_init_heap(16);
+	int i;
 	for (i = 1; i <= 16; i++)
 		bh_insert(17-i, NULL, h);
 	i = 0;
--- p1_heap/src/FibonacciHeap.c
+++ p1_heap/src/FibonacciHeap.c
@@ -15,9 +15,11 @@ void *fib_delete(FibNode *node, FibHeap *heap);
 
 
 
-FibHeap *fib_make_heap(unsigned int maxRank) {
-	FibHeap *heap = calloc(1,sizeof(struct FibHeap));
+FibHeap *fib_make_heap(unsigned int maxRank)
+{
+	FibHeap *heap = malloc(sizeof(struct FibHeap));
 	heap->maxRank = maxRank;
+	heap->min = NULL;
 	return heap;
 }
 
@@ -25,9 +27,10 @@ FibNode *fib_find_min(FibHeap *heap) {
 	return heap->min;
 }
 
-FibNode *fib_insert(unsigned int key, FibHeap *heap) {
+FibNode *fib_insert(unsigned int key, FibHeap *heap)
+{
 	FibNode *node;
-	if(heap->min == NULL) {
+	if(!heap->min) {
 		node = calloc(1,sizeof(struct FibNode));
 		node->key = key;
 		fib_link(node, node);
@@ -41,7 +44,8 @@ FibNode *fib_insert(unsigned int key, FibHeap *heap) {
 	return node;
 }
 
-void *fib_meld(FibHeap *heap1, FibHeap *heap2) {
+void *fib_meld(FibHeap *heap1, FibHeap *heap2)
+{
 	fib_union(heap1->min, heap2->min);
 	if(heap1->min->key > heap2->min->key)
 		heap1->min = heap2->min;
@@ -49,15 +53,17 @@ void *fib_meld(FibHeap *heap1, FibHeap *heap2) {
 		heap2->min = heap1->min;
 }
 
-void *fib_link(FibNode *left, FibNode *right) {
+void *fib_link(FibNode *left, FibNode *right)
+{
 	left->right = right;
 	right->left = left;
 }
 
-void *fib_union(FibNode *node1, FibNode *node2) {
-	if(node1 == NULL)
+void *fib_union(FibNode *node1, FibNode *node2)
+{
+	if(!node1)
 		node1 = node2;
-	if(node2 == NULL)
+	if(!node2)
 		node2 = node1;
 	node1->left->right = node2->right;
 	node2->right->left = node1->left;
@@ -65,7 +71,8 @@ void *fib_union(FibNode *node1, FibNode *node2) {
 	node1->left = node2;
 }
 
-void *fib_delete_min(FibHeap *heap) {
+void *fib_delete_min(FibHeap *heap)
+{
 	fib_union(heap->min->child, heap->min->left);
 
 	FibNode *startNode;
@@ -90,7 +97,8 @@ void *fib_delete_min(FibHeap *heap) {
 	free(rootNodes);
 }
 
-void *fib_extract_childnode(FibNode *node, FibHeap *heap) {
+void *fib_extract_childnode(FibNode *node, FibHeap *heap)
+{
 	FibNode *parent = node->parent;
 	if(node->left == node) {
 		parent->child = NULL;
@@ -108,7 +116,8 @@ void *fib_extract_childnode(FibNode *node, FibHeap *heap) {
 	parent->marked = !parent->marked;
 }
 
-void *fib_decrease_key(unsigned int delta, FibNode *node, FibHeap *heap) {
+void *fib_decrease_key(unsigned int delta, FibNode *node, FibHeap *heap)
+{
 	node->key -= delta;
 	if(node->parent != NULL) {
 		fib_extract_childnode(node, heap);
@@ -118,7 +127,8 @@ void *fib_decrease_key(unsigned int delta, FibNode *node, FibHeap *heap) {
 		heap->min = node;
 }
 
-void *fib_delete(FibNode *node, FibHeap *heap) {
+void *fib_delete(FibNode *node, FibHeap *heap)
+{
 	if(heap->min == node) {
 		fib_delete_min(heap);
 	} else {
