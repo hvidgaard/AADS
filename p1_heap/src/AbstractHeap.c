eb586e3bffaf5b2c2b77215199e6d4c3875f2cbf
Anders Ingemann <anders@ingemann.de>
2011-09-19 18:12:19 +0200

    New abstract heap, makes it easier when testing

---
+++ p1_heap/src/AbstractHeap.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <AbstractHeap.h>
+
+/* Initialize a new Fibonacci heap. */
+AbstractHeap *make_heap(unsigned int which, unsigned int size)
+{
+	struct AbstractHeap *heap = calloc(1,sizeof *heap);
+	if(which == USE_BINARY_HEAP)
+		heap->bin = bh_init_heap(size);
+	else
+		heap->fib = fib_make_heap();
+	return heap;
+}
+
+AbstractNode *find_min(AbstractHeap *heap) {
+	AbstractNode *node = calloc(1,sizeof(struct AbstractNode));
+	if (heap->bin)
+		node->bin = bh_find_min(heap->bin);
+	else
+		node->fib = fib_find_min(heap->fib);
+	return node;
+}
+
+AbstractNode *insert(unsigned int key, void *data, AbstractHeap *heap) {
+	AbstractNode *node = calloc(1,sizeof(struct AbstractNode));
+	if (heap->bin)
+		node->bin = bh_insert(key, data, heap->bin);
+	else
+		node->fib = fib_insert(key, heap->fib);
+	node->key = key;
+	node->data = data;
+	return node;
+}
+
+void *delete_min(AbstractHeap *heap) {
+	if(heap->bin)
+		bh_delete_min(heap->bin);
+	else
+		fib_delete_min(heap->fib);
+}
+
+void *decrease_key(unsigned int delta, AbstractNode *node, AbstractHeap *heap) {
+	if(heap->bin)
+		bh_decrease_key(delta, node->bin, heap->bin);
+	else
+		fib_decrease_key(delta, node->fib, heap->fib);
+	node->key -= delta;
+}
+void *delete(AbstractNode *node, AbstractHeap *heap) {
+	if(heap->bin)
+		bh_delete_element(node->bin, heap->bin);
+	else
+		fib_delete(node->fib, heap->fib);
+}
