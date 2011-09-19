3ad7bf490bd07f7d422de4939718447a69313ed7
Anders Ingemann <anders@ingemann.de>
2011-09-19 17:54:34 +0200

    Removed the Test files and the main methods in the heaps. Seperate test file coming up

--- p1_heap/src/FibonacciHeap.c
+++ p1_heap/src/FibonacciHeap.c
@@ -219,22 +219,3 @@ void *fib_delete(FibNode *node, FibHeap *heap)
 		free(node);
 	}
 }
-
-int main(int argc, char **argv)
-{
-	FibHeap *heap = fib_make_heap(30);
-	int i;
-	for (i = 1; i <= 16; i++)
-		fib_insert(17-i, heap);
-	
-	FibNode *twenty = fib_insert(21, heap);
-	fib_decrease_key(15, twenty, heap);
-
-	FibNode *min = (FibNode *) fib_find_min(heap);
-	while (min != NULL) {
-		printf("min is %d\n", min->key);
-		fib_delete_min(heap);
-		min = fib_find_min(heap);
-	}
-	return 0;
-}
