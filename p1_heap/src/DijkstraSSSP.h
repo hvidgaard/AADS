62bd53b4fec08ae85a7135e8ad7fecc78201a2a4
Peter Hvidgaard <hvidgaard@gmail.com>
2011-09-26 16:58:10 +0200

    fisk

--- p1_heap/src/DijkstraSSSP.h
+++ p1_heap/src/DijkstraSSSP.h
@@ -4,6 +4,7 @@
 #define UINT_MAX 429496729
 
 unsigned int *generate_graph(unsigned int vertices, unsigned int edge_chance, unsigned int max_weight, unsigned int seed);
+unsigned int *generate_decrease_key_max(unsigned int vertices);
 unsigned int *dijkstra_bin(unsigned int num_vertices, unsigned int source, unsigned int * w, unsigned int ** edges);
 unsigned int *dijkstra_fib(unsigned int num_vertices, unsigned int source, unsigned int * w, unsigned int ** edges);
 unsigned int *dijkstra_pq(unsigned int num_vertices, unsigned int source, unsigned int * w, unsigned int ** edges);
