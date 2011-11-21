c81ab6ad374429b8db1f5736029b8b6fb637f824
Anders Ingemann <anders@ingemann.de>
2011-11-21 20:18:42 +0100

    Graphs for more than one measure...

--- p2_vEBTrees/benchmark/generate_graphs.php
+++ p2_vEBTrees/benchmark/generate_graphs.php
@@ -16,43 +16,46 @@ $abs_file = 'logs/concatenated.abs.averages';
 
 $binary = new stdClass;
 $binary->name = 'Binary';
-$binary->color = '#555599';
+$binary->color = '#6c71c4';
 $binary->selector = 'bin';
 
 $fibonacci = new stdClass;
 $fibonacci->name = 'Fibonacci';
-$fibonacci->color = '#995599';
+$fibonacci->color = '#002b36';
 $fibonacci->selector = 'fib';
 
 $veb = new stdClass;
 $veb->name = 'van Emde Boas';
-$veb->color = '#CCCC55';
+$veb->color = '#b58900';
 $veb->selector = 'veb';
 
 $redblack = new stdClass;
 $redblack->name = 'Red Black';
-$redblack->color = '#55CCCC';
+$redblack->color = '#dc322f';
 $redblack->selector = 'rbt';
 
 $stdline = 'lines linewidth 2 linecolor rgb';
 
+$measurements = array('cycles' => 'logs/concatenated.cyc.averages', 'absolute' => 'logs/concatenated.abs.averages');
 $columns = array(3 => 'minimum', 4 => 'maximum', 5 => 'averages', 6 => 'standard_deviation', 7 => 'samples');
 $generators = array('random' => 'Random', /*'dkmax' => 'Decrease Key maximized', */'dkmax2' => 'Decrease Key maximized v2');
-$algorithms = array($binary, $fibonacci, $veb, $redblack);
-foreach($columns as $column => $columnName) {
-	foreach($generators as $generator => $generatorName)  {
-		$graph = new Graph;
-		$graph->title = "\"$generatorName graph $columnName\"";
-		foreach($algorithms as $algo) {
-			$plot = new Plot;
-			$plot->datafile = "< grep \"{$algo->selector}_{$generator}\" $cyc_file";
-			$plot->datamodifiers = "using 2:$column";
-			$plot->style = "$stdline '$algo->color'";
-			$plot->title = $algo->name;
-			$graph->addPlot($plot);
+$algorithms = array($binary, $fibonacci, $veb/*, $redblack*/);
+foreach($measurements as $measurement => $file) {
+	foreach($columns as $column => $columnName) {
+		foreach($generators as $generator => $generatorName)  {
+			$graph = new Graph;
+			$graph->title = "\"$generatorName graph $columnName\"";
+			foreach($algorithms as $algo) {
+				$plot = new Plot;
+				$plot->datafile = "< grep \"{$algo->selector}_{$generator}\" $file";
+				$plot->datamodifiers = "using 2:$column";
+				$plot->style = "$stdline '$algo->color'";
+				$plot->title = $algo->name;
+				$graph->addPlot($plot);
+			}
+			$graph->output("graphs/{$generator}_{$columnName}_{$measurement}.png");
+			$graph->terminal = "epslatex";
+			$graph->output("graphs/{$generator}_{$columnName}_{$measurement}.eps");
 		}
-		$graph->output("graphs/{$generator}_{$columnName}.png");
-		$graph->terminal = "epslatex";
-		$graph->output("graphs/{$generator}_{$columnName}.eps");
 	}
 }
